68d69a22bb96711a259c1c591822191f199cf975
Lucas Paiva <lpaiva234@gmail.com>
2025-03-18 14:51:54 -0300

    add solution to exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let stringReversed = "";
 
+    stringAsArray = string.split("");
+    arrayReversed = stringAsArray.reverse();
+    stringReversed = arrayReversed.join("");
+
+    return stringReversed
 };
 
 // Do not edit below this line
