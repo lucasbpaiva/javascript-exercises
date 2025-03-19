8c7c03c14e5017cda494b71b9717f134b36044e9
Lucas Paiva <lpaiva234@gmail.com>
2025-03-19 11:06:00 -0300

    add solution to exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,8 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    if (year % 4) return false;
+    if (year % 100) return true;
+    if (year % 400) return false;
+    return true;
 };
 
 // Do not edit below this line
