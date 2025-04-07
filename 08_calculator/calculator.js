26c69769d85df156af8a23f282efba9554031b9e
Lucas Paiva <lpaiva234@gmail.com>
2025-04-07 15:48:29 -0300

    add solution to exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,26 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((sum, current) => sum + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	return array.reduce((product, current) => product * current, 1);
 };
 
-const power = function() {
-	
+const power = function(base, exp) {
+	return base ** exp;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n == 0) return 1;
+  return n * factorial(n - 1);
 };
 
 // Do not edit below this line
