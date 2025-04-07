86a51c3cdccbd890a3dce2e9c31793e8703140ad
Lucas Paiva <lpaiva234@gmail.com>
2025-04-07 19:47:28 -0300

    add solution to exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,18 +1,19 @@
 const lettersAndNumbers = "abcdefghijklmnopqrstuvwxyz0123456789";
 
 const palindromes = function (string) {
-    let arrayOfString = string
+    let cleanedString = string
     .toLowerCase()
     .split("")
-    .filter(char => lettersAndNumbers.includes(char));
-    
-    string = arrayOfString.join(""); //original string without punctuation
+    .filter(char => lettersAndNumbers.includes(char))
+    .join("");
 
-    stringReversed = arrayOfString.reverse().join("");
-    console.log(stringReversed)
-    return string == stringReversed;
+    stringReversed = cleanedString
+    .split("")
+    .reverse()
+    .join("");
+
+    return cleanedString == stringReversed;
 };
-
 
 // Do not edit below this line
 module.exports = palindromes;
