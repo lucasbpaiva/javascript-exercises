aa5b850d045f10bf955f40a5747a3da75e74c751
Lucas Paiva <lpaiva234@gmail.com>
2025-04-08 10:38:38 -0300

    add solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
+function getAge(person) {
+    person.yearOfDeath ??= new Date().getFullYear();
+    return person.yearOfDeath - person.yearOfBirth;
+}
 
+const findTheOldest = function(people) {
+    return people
+    .slice(0)
+    .sort((a, b) => getAge(b) - getAge(a))
+    .at(0);
 };
 
 // Do not edit below this line
